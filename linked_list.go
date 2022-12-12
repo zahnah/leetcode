47d226d08da55f5deb53da0694547d2c4b56d3e2
zahnah <zakharov.vp@gmail.com>
2022-12-12 16:12:57 +0300

    #19. Remove Nth Node From End of List

--- linked_list.go
+++ linked_list.go
@@ -16,3 +16,24 @@ func MiddleNode(head *ListNode) *ListNode {
 	}
 	return middle
 }
+
+// RemoveNthFromEnd
+// 19. Remove Nth Node From End of List
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+func RemoveNthFromEnd(head *ListNode, n int) *ListNode {
+	next := head
+	end := head
+	for i := 0; i < n && end != nil; i++ {
+		end = end.Next
+	}
+	if end == nil {
+		head = head.Next
+		return head
+	}
+	for end.Next != nil {
+		next = next.Next
+		end = end.Next
+	}
+	next.Next = next.Next.Next
+	return head
+}
--- linked_list_test.go
+++ linked_list_test.go
@@ -1,14 +1,26 @@
 package leetcode
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
+
+func ToString(node *ListNode) string {
+	result := ""
+	for node != nil {
+		result = result + fmt.Sprint(node.Val)
+		node = node.Next
+	}
+	return result
+}
 
 type MiddleNodeTest struct {
 	arg1     *ListNode
 	expected int
 }
 
-var MiddleNodeTests = []MiddleNodeTest{
-	{arg1: &ListNode{
+func getNode() *ListNode {
+	return &ListNode{
 		Val: 0,
 		Next: &ListNode{
 			Val: 1,
@@ -26,7 +38,11 @@ var MiddleNodeTests = []MiddleNodeTest{
 				},
 			},
 		},
-	}, expected: 3},
+	}
+}
+
+var MiddleNodeTests = []MiddleNodeTest{
+	{arg1: getNode(), expected: 3},
 }
 
 // ReverseString
@@ -35,6 +51,41 @@ func TestMiddleNode(t *testing.T) {
 		result := MiddleNode(test.arg1)
 		if result.Val != test.expected {
 			t.Errorf("Output %q not equal to expected %q", result.Val, test.expected)
+		}
+	}
+}
+
+type RemoveNthFromEndTest struct {
+	arg1     *ListNode
+	arg2     int
+	expected string
+}
+
+var RemoveNthFromEndTests = []RemoveNthFromEndTest{
+	{arg1: getNode(), arg2: 2, expected: "01235"},
+	{arg1: getNode(), arg2: 3, expected: "01245"},
+	{arg1: &ListNode{
+		Val: 1,
+	}, arg2: 1, expected: ""},
+	{arg1: &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+		},
+	}, arg2: 1, expected: "1"},
+	{arg1: &ListNode{
+		Val: 1,
+		Next: &ListNode{
+			Val: 2,
+		},
+	}, arg2: 2, expected: "2"},
+}
+
+func TestRemoveNthFromEnd(t *testing.T) {
+	for _, test := range RemoveNthFromEndTests {
+		result := RemoveNthFromEnd(test.arg1, test.arg2)
+		if ToString(result) != test.expected {
+			t.Errorf("Output %q not equal to expected %q", ToString(result), test.expected)
 		}
 	}
 }
